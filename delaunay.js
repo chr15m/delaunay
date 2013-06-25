a204baccc91d15e18d04bd5f62e68db9432930d1
Chris McCormick <chris@mccormick.cx>
2013-06-25 21:26:58 +0800

    Namespaced the public functions and methods so they are safer in the browser. Fixed semicolon and curlybrace ambiguity.

--- delaunay.js
+++ delaunay.js
@@ -1,7 +1,7 @@
-function Triangle(a, b, c) {
-  this.a = a
-  this.b = b
-  this.c = c
+function DelaunayTriangle(a, b, c) {
+  this.a = a;
+  this.b = b;
+  this.c = c;
 
   var A = b.x - a.x,
       B = b.y - a.y,
@@ -10,83 +10,72 @@ function Triangle(a, b, c) {
       E = A * (a.x + b.x) + B * (a.y + b.y),
       F = C * (a.x + c.x) + D * (a.y + c.y),
       G = 2 * (A * (c.y - b.y) - B * (c.x - b.x)),
-      minx, miny, dx, dy
+      minx, miny, dx, dy;
 
   /* If the points of the triangle are collinear, then just find the
    * extremes and use the midpoint as the center of the circumcircle. */
   if(Math.abs(G) < 0.000001) {
-    minx = Math.min(a.x, b.x, c.x)
-    miny = Math.min(a.y, b.y, c.y)
-    dx   = (Math.max(a.x, b.x, c.x) - minx) * 0.5
-    dy   = (Math.max(a.y, b.y, c.y) - miny) * 0.5
+    minx = Math.min(a.x, b.x, c.x);
+    miny = Math.min(a.y, b.y, c.y);
+    dx   = (Math.max(a.x, b.x, c.x) - minx) * 0.5;
+    dy   = (Math.max(a.y, b.y, c.y) - miny) * 0.5;
 
-    this.x = minx + dx
-    this.y = miny + dy
-    this.r = dx * dx + dy * dy
+    this.x = minx + dx;
+    this.y = miny + dy;
+    this.r = dx * dx + dy * dy;
   }
 
   else {
-    this.x = (D*E - B*F) / G
-    this.y = (A*F - C*E) / G
-    dx = this.x - a.x
-    dy = this.y - a.y
-    this.r = dx * dx + dy * dy
+    this.x = (D*E - B*F) / G;
+    this.y = (A*F - C*E) / G;
+    dx = this.x - a.x;
+    dy = this.y - a.y;
+    this.r = dx * dx + dy * dy;
   }
 }
 
-Triangle.prototype.draw = function(ctx) {
-  ctx.beginPath()
-  ctx.moveTo(this.a.x, this.a.y)
-  ctx.lineTo(this.b.x, this.b.y)
-  ctx.lineTo(this.c.x, this.c.y)
-  ctx.closePath()
-  ctx.stroke()
-}
-
-function byX(a, b) {
-  return b.x - a.x
-}
-
-function dedup(edges) {
+function delaunay_dedup(edges) {
   var j = edges.length,
-      a, b, i, m, n
+      a, b, i, m, n;
 
   outer: while(j) {
-    b = edges[--j]
-    a = edges[--j]
-    i = j
+    b = edges[--j];
+    a = edges[--j];
+    i = j;
     while(i) {
-      n = edges[--i]
-      m = edges[--i]
+      n = edges[--i];
+      m = edges[--i];
       if((a === m && b === n) || (a === n && b === m)) {
-        edges.splice(j, 2)
-        edges.splice(i, 2)
-        j -= 2
-        continue outer
+        edges.splice(j, 2);
+        edges.splice(i, 2);
+        j -= 2;
+        continue outer;
       }
     }
   }
 }
 
-function triangulate(vertices) {
+function delaunay_triangulate(vertices) {
   /* Bail if there aren't enough vertices to form any triangles. */
   if(vertices.length < 3)
-    return []
+    return [];
 
   /* Ensure the vertex array is in order of descending X coordinate
    * (which is needed to ensure a subquadratic runtime), and then find
    * the bounding box around the points. */
-  vertices.sort(byX)
+  vertices.sort(function(a, b) {
+    return b.x - a.x;
+  });
 
   var i    = vertices.length - 1,
       xmin = vertices[i].x,
       xmax = vertices[0].x,
       ymin = vertices[i].y,
-      ymax = ymin
+      ymax = ymin;
 
   while(i--) {
-    if(vertices[i].y < ymin) ymin = vertices[i].y
-    if(vertices[i].y > ymax) ymax = vertices[i].y
+    if(vertices[i].y < ymin) ymin = vertices[i].y;
+    if(vertices[i].y > ymax) ymax = vertices[i].y;
   }
 
   /* Find a supertriangle, which is a triangle that surrounds all the
@@ -103,7 +92,7 @@ function triangulate(vertices) {
       xmid   = (xmax + xmin) * 0.5,
       ymid   = (ymax + ymin) * 0.5,
       open   = [
-        new Triangle(
+        new DelaunayTriangle(
           {x: xmid - 20 * dmax, y: ymid -      dmax, __sentinel: true},
           {x: xmid            , y: ymid + 20 * dmax, __sentinel: true},
           {x: xmid + 20 * dmax, y: ymid -      dmax, __sentinel: true}
@@ -111,31 +100,31 @@ function triangulate(vertices) {
       ],
       closed = [],
       edges = [],
-      j, a, b
+      j, a, b;
 
   /* Incrementally add each vertex to the mesh. */
-  i = vertices.length
+  i = vertices.length;
   while(i--) {
     /* For each open triangle, check to see if the current point is
      * inside it's circumcircle. If it is, remove the triangle and add
      * it's edges to an edge list. */
-    edges.length = 0
-    j = open.length
+    edges.length = 0;
+    j = open.length;
     while(j--) {
       /* If this point is to the right of this triangle's circumcircle,
        * then this triangle should never get checked again. Remove it
        * from the open list, add it to the closed list, and skip. */
-      dx = vertices[i].x - open[j].x
+      dx = vertices[i].x - open[j].x;
       if(dx > 0 && dx * dx > open[j].r) {
-        closed.push(open[j])
-        open.splice(j, 1)
-        continue
+        closed.push(open[j]);
+        open.splice(j, 1);
+        continue;
       }
 
       /* If not, skip this triangle. */
       dy = vertices[i].y - open[j].y
       if(dx * dx + dy * dy > open[j].r)
-        continue
+        continue;
 
       /* Remove the triangle and add it's edges to the edge list. */
       edges.push(
@@ -143,39 +132,41 @@ function triangulate(vertices) {
         open[j].b, open[j].c,
         open[j].c, open[j].a
       )
-      open.splice(j, 1)
+      open.splice(j, 1);
     }
 
     /* Remove any doubled edges. */
-    dedup(edges)
+    delaunay_dedup(edges);
 
     /* Add a new triangle for each edge. */
     j = edges.length
     while(j) {
-      b = edges[--j]
-      a = edges[--j]
-      open.push(new Triangle(a, b, vertices[i]))
+      b = edges[--j];
+      a = edges[--j];
+      open.push(new DelaunayTriangle(a, b, vertices[i]));
     }
   }
 
   /* Copy any remaining open triangles to the closed list, and then
    * remove any triangles that share a vertex with the supertriangle. */
-  Array.prototype.push.apply(closed, open)
+  Array.prototype.push.apply(closed, open);
 
-  i = closed.length
-  while(i--)
+  i = closed.length;
+  while(i--) {
     if(closed[i].a.__sentinel ||
        closed[i].b.__sentinel ||
-       closed[i].c.__sentinel)
-      closed.splice(i, 1)
+       closed[i].c.__sentinel) {
+      closed.splice(i, 1);
+    }
+  }
 
   /* Yay, we're done! */
-  return closed
+  return closed;
 }
 
 if (typeof module !== 'undefined') {
     module.exports = {
-        Triangle: Triangle,
-        triangulate: triangulate
+        Triangle: DelaunayTriangle,
+        triangulate: delaunay_triangulate
     }
 }
